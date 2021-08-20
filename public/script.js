039904a024752f016eef156ac308791bfd7939d5
Aryan-369 <71432096+Aryan369@users.noreply.github.com>
2021-08-20 18:33:32 +0530

    Updated

--- public/script.js
+++ public/script.js
@@ -22,21 +22,18 @@ msgform.addEventListener('submit', e =>{
     e.preventDefault();
     const message = msgInp.value;
     sendMsg(message);
-    //socket.emit('send-chat-message', message);
     //msgInp.value = "";
 });
 
 function sendMsg(message){
     let msg = {
         user: "codename",
-        message: message
+        message: message.trim()
     }
 
     appendMsg(msg, "outgoing", "out");
-
-    // const msgElement = document.createElement("div");
-    // msgElement.innerText = msg;
-    // msgContainer.appendChild()
+    
+    socket.emit('send-chat-message', msg);
 }
 
 function appendMsg(msg, divtype, pType){
