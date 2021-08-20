bceb98f1887f0c3e2024f8be91840662870ed6d9
Aryan-369 <71432096+Aryan369@users.noreply.github.com>
2021-08-20 19:17:21 +0530

    Updated

--- public/rooms/assets/js/client.js
+++ public/rooms/assets/js/client.js
@@ -1,12 +1,12 @@
-const _codename = require("../../../../app");
+//const _codename = require("../../../../app");
 const socket = io();
 
 const msgform = document.getElementById('send-container');
 const msgInp = document.getElementById("msgInp");
 const msgContainer = document.querySelector(".msg_area");
 
-const codename = _codename;
-//const codename = prompt("Enter your name");
+//const codename = _codename;
+const codename = prompt("Enter your name");
 JoinedMsg("You");
 
 //Joining Info
