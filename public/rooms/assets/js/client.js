6cb2523633efb266bba3ba1f47a475b3887c6557
Aryan-369 <71432096+Aryan369@users.noreply.github.com>
2021-08-20 19:06:47 +0530

    Ipdated

--- public/rooms/assets/js/client.js
+++ public/rooms/assets/js/client.js
@@ -1,19 +1,84 @@
-const e = require("cors");
-
 //const _codename = require("./../../../../app");
-const socket = io("http://localhost:3000");
+const socket = io();
 
 const msgform = document.getElementById('send-container');
-//const msgInp = document.getElementById("msgInp");
-//const msgContainer = document.querySelector(".msg_area");
+const msgInp = document.getElementById("msgInp");
+const msgContainer = document.querySelector(".msg_area");
 
 //const codename = _codename;
-//const _name = prompt("enter your name");
+const _name = prompt("Enter your name");
+JoinedMsg("You");
 
-socket.on('chat-message', data => {
-    console.log(data);
+//Joining Info
+socket.emit('new-user-joined', _name);
+
+socket.on('member-joined', codeName => {
+    JoinedMsg(codeName);
+});
+
+
+
+msgInp.addEventListener('keyup', e => {
+    if(e.key === "Enter"){
+        sendMsg(e.target.value);
+    }
 });
 
 msgform.addEventListener('submit', e =>{
     e.preventDefault();
+    const message = msgInp.value;
+    sendMsg(message);
 });
+
+
+//Send Messages
+function sendMsg(message){
+    let msg = {
+        user: "codename",
+        message: message.trim()
+    }
+
+    appendMsg(msg, "outgoing", "out");
+    msgInp.value = "";
+    scrollToBottom();
+    
+    socket.emit('send-chat-message', msg);
+}
+
+
+//Receive Messages
+socket.on('chat-message', msg => {
+    appendMsg(msg, "incoming", "in");
+    scrollToBottom();
+});
+
+
+function appendMsg(msg, divtype, pType){
+    let mainDiv = document.createElement("div");
+    let divClass = divtype;
+    mainDiv.classList.add(divClass, "msg");
+
+    let markup = `
+        <h4>${msg.user}</h4>
+        <p class="${pType}">${msg.message}</p>
+    `
+
+    mainDiv.innerHTML = markup;
+    msgContainer.appendChild(mainDiv);
+}
+
+function JoinedMsg(member){
+    let mainDiv = document.createElement("div");
+    mainDiv.classList.add("joined", "msg");
+
+    let markup = `
+        <h4>${member} joined the room.</h4>
+    `
+
+    mainDiv.innerHTML = markup;
+    msgContainer.appendChild(mainDiv);
+}
+
+function scrollToBottom() {
+    msgContainer.scrollTop = msgContainer.scrollHeight;
+}
