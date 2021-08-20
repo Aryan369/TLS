94b12e906e6402a3c671e94d8e711b201984a070
Aryan-369 <71432096+Aryan369@users.noreply.github.com>
2021-08-20 17:57:22 +0530

    Updated

--- test.js
+++ test.js
@@ -1,4 +1,18 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+app.use(require('cors')())
+
+const http = require("http").createServer(app);
 const io = require("socket.io")(3000);
+
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/test.html`);
+});
+
+http.listen(4000, function(){
+    console.log("Successfully Connected Server");
 
 io.on('connection', socket => {
     //socket.on('new-user-joined', codename => {
@@ -10,4 +24,5 @@ io.on('connection', socket => {
     socket.on('send-chat-message', message => {
         socket.broadcast.emit('chat-message' ,message);
     })
-})
+});
+});
