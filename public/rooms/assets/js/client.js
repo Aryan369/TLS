6ead9eec71d7d37a676f81068d710479e5d2f39f
Aryan-369 <71432096+Aryan369@users.noreply.github.com>
2021-08-20 13:55:30 +0530

    Updated

---
+++ public/rooms/assets/js/client.js
@@ -0,0 +1,5 @@
+const socket = io(5500);
+
+const form = document.getElementById("send-form");
+const msgInp = document.getElementById("msgInp");
+const msgContainer = document.querySelector(".msg_area");
--- test.js
+++ test.js
@@ -1,4 +1,13 @@
 const io = require("socket.io")(process.env.PORT || 5500);
+const express = require("express");
+const ejs = require("ejs");
+const RoomsRoute = require("./routes/rooms");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use("/", RoomsRoute);
 
 const users = {};
 
@@ -12,3 +21,5 @@ io.on('connect', socket => {
         socket.broadcast.emit('receive', {message: message, codename: user[socket.id]});
     });
 })
+
+app.listen("3000");
