e44157da5a7171c379a621a7f21f24dabfa5be47
Aryan-369 <71432096+Aryan369@users.noreply.github.com>
2021-08-20 14:37:08 +0530

    Updated

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const socket = io("http://localhost:3000");
 
 const msgform = document.getElementById('send-container');
-//const msgInp = document.getElementById("msgInp");
+const msgInp = document.getElementById("msgInp");
 //const msgContainer = document.querySelector(".msg_area");
 
 //const codename = _codename;
@@ -14,4 +14,7 @@ socket.on('chat-message', data => {
 
 msgform.addEventListener('submit', e =>{
     e.preventDefault();
+    const message = msgInp.value;
+    socket.emit('send-chat-message', message);
+    msgInp.value = "";
 });
