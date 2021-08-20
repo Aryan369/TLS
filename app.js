596e48b3540aeb24124bc25eea4387e9eacb5824
Aryan-369 <71432096+Aryan369@users.noreply.github.com>
2021-08-20 19:08:20 +0530

    Updated

--- app.js
+++ app.js
@@ -72,50 +72,10 @@ app.use("/", HomepageRoute);
 
 app.use("/homepageContact", HomepageContactRoute);
 
-app.get("/secrets", function(req, res){
-  User.find({"secret": {$ne: null}}, function(err, foundUsers){
-    if (err){
-      console.log(err);
-    } else {
-      if (foundUsers) {
-        res.render("secrets", {usersWithSecrets: foundUsers});
-      }
-    }
-  });
-});
-
 //Chats
 app.use("/rooms", RoomsRoute)
 
 app.use("/hq", HQRoute);
-
-app.get("/submit", function(req, res){
-  if (req.isAuthenticated()){
-    res.render("submit");
-  } else {
-    res.redirect("/login");
-  }
-});
-
-app.post("/submit", function(req, res){
-  const submittedSecret = req.body.secret;
-
-//Once the user is authenticated and their session gets saved, their user details are saved to req.user.
-  // console.log(req.user.id);
-
-  User.findById(req.user.id, function(err, foundUser){
-    if (err) {
-      console.log(err);
-    } else {
-      if (foundUser) {
-        foundUser.secret = submittedSecret;
-        foundUser.save(function(){
-          res.redirect("/secrets");
-        });
-      }
-    }
-  });
-});
 
 app.use("/logout", LogoutRoute);
 
