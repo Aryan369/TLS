81e7d84eeae9bb9982e39af795fd28c6df6aa387
Aryan-369 <71432096+Aryan369@users.noreply.github.com>
2021-08-20 14:18:26 +0530

    Updated

--- public/rooms/assets/js/client.js
+++ public/rooms/assets/js/client.js
@@ -1,10 +1,11 @@
-const _codename = require("./../../../../app");
+//const _codename = require("./../../../../app");
 const socket = io(5500);
 
 const form = document.getElementById("send-form");
 const msgInp = document.getElementById("msgInp");
 const msgContainer = document.querySelector(".msg_area");
 
-const codename = _codename;
+//const codename = _codename;
+const name = "name";
 
-socket.emit('new-user-joined', codename);
+socket.emit('new-user-joined', name);
--- test.js
+++ test.js
@@ -1,27 +1,15 @@
 const io = require("socket.io")(8000);
-const express = require("express");
-const ejs = require("ejs");
-const RoomsRoute = require("./routes/rooms");
-
-const app = express();
-
-app.use(express.static("public"));
-app.set('view engine', 'ejs');
-
-app.use("/", RoomsRoute);
 
 const users = {};
 
 io.on('connect', socket => {
-    socket.on('new-user-joined', codename => {
-        console.loh(codename);
-        users[socket.id] = codename;
-        socket.broadcast.emit('member-joined', codename);
+    socket.on('new-user-joined', name => {
+        console.log(name);
+        users[socket.id] = name;
+        socket.broadcast.emit('member-joined', name);
     });
 
     socket.on('send', message => {
         socket.broadcast.emit('receive', {message: message, codename: user[socket.id]});
     });
 })
-
-app.listen("3000");
